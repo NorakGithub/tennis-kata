9fc243f56ef9685a9efb7088996f96b71de8025f
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-08-19 13:48:02 +0700

    Fixed test env

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
 //
 // Your implementations
-// 
+//
+
+class Game {
+    constructor(
+        private p1Score: number,
+        private p2Score: number,
+    ){}
+}
+
+export function updateGameScore(p1Score: number, p2Score: number): Game {
+    return new Game(p1Score, p2Score);
+}
+
+export function decideGameWinner(game: Game): string {
+    return 'hello world';
+}
+
+export function printScore(scores: number[]): string {
+    return 'hello world';
+}
