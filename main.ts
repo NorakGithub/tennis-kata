16093742888f90d71331f136b94d1e43f093db86
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-08-19 14:06:33 +0700

    Update main for following the test

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@
 
 class Game {
     constructor(
-        private p1Score: number,
-        private p2Score: number,
+        public p1Score: number,
+        public p2Score: number,
     ){}
 }
 
@@ -14,9 +14,15 @@ export function updateGameScore(p1Score: number, p2Score: number): Game {
 }
 
 export function decideGameWinner(game: Game): string {
-    return 'hello world';
+    return game.p1Score > game.p2Score ? 'Player 1' : 'Player 2';
 }
 
 export function printScore(scores: number[]): string {
-    return 'hello world';
+    if (scores[0] === 3 && scores[1] === 3) {
+        return 'deuce'
+    }
+    if (scores[0] === 2 && scores[1] === 2) {
+        return 'thirty - thirty'
+    }
+    return scores[0] > scores[1] ? 'advantage player 1' : 'advantage player 2';
 }
